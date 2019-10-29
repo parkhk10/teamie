3b08593b93d0ed9be062ea70e1b23f19ffe09199
Keren Park <parkhk10@gmail.com>
2019-10-28 22:35:48 -0500

    test another change

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -95,7 +95,7 @@ const AppBar_header =  ({numPeople, setNumPeople, setBudget, setVibe}) => {
       <Container>
         <Toolbar>
           <Typography variant="h6" className={classes.title}>
-            Welcome to Teamie!
+            Change Title Again
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
