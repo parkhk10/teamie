0ea05217d6e141395535c74925855cfc9ac51a91
Keren Park <parkhk10@gmail.com>
2019-10-28 22:29:38 -0500

    change title

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -95,7 +95,7 @@ const AppBar_header =  ({numPeople, setNumPeople, setBudget, setVibe}) => {
       <Container>
         <Toolbar>
           <Typography variant="h6" className={classes.title}>
-            Teamie CI!
+            Welcome to Teamie!
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
