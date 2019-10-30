a06135b08884a86ed850a712795ec7c1b559ceb3
Keren Park <parkhk10@gmail.com>
2019-10-30 13:50:28 -0500

    another change

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -95,7 +95,7 @@ const AppBar_header =  ({numPeople, setNumPeople, setBudget, setVibe}) => {
       <Container>
         <Toolbar>
           <Typography variant="h6" className={classes.title}>
-            I hope this works now
+            HELLO
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
