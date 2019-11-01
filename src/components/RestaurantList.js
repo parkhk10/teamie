a6f24510c35103a58fb08934c7c61284958a02fd
Keren Park <parkhk10@gmail.com>
2019-11-01 13:22:45 -0500

    CHANGE TITLE

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -95,7 +95,7 @@ const AppBar_header =  ({numPeople, setNumPeople, setBudget, setVibe}) => {
       <Container>
         <Toolbar>
           <Typography variant="h6" className={classes.title}>
-            HELLO!!!
+            HELP!!!
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
