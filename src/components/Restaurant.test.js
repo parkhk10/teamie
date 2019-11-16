f5e56988eea661abfaa597cefebe39521ada5277
Keren Park <parkhk10@gmail.com>
2019-11-16 11:17:10 -0600

    reset tests

--- src/components/Restaurant.test.js
+++ src/components/Restaurant.test.js
@@ -4,19 +4,7 @@ import Restaurant from './Restaurant';
 
 test('Restaurant should render passed states to display restaurant cards', () => {
   
-    let key = "1"
-    let restaurant = {id: "1", name: "Koco Table", type: "Korean", price: 14, start: "1100", end: "1350",
-                      tables: [2, 4, 6, 10], party_size: ["small", "medium", "large"], vibes: ["happy_hour", "good_for_clients", "family_friendly", "team_bonding"]}
-    let selectedRestaurants = [];
-    let setSelectedRestaurants = jest.fn()
-
-    const {getByTestId} = render(<Restaurant key={key} restaurant={restaurant}
-                                               selectedRestaurants={selectedRestaurants}
-                                               setSelectedRestaurants={setSelectedRestaurants}/>)
-  
-    expect(getByTestId("restaurant1").textContent).toBe("Koco Table");
-  
-    // fireEvent.click(getByTestId('markAsCompleted'))
+    // fireEvent.click(getByTestId('addToPoll'))
     // expect(markTodoDone).toBeCalledWith(itemIndex)
     // expect(markTodoDone).toHaveBeenCalledTimes(1)
   
