82bdb99302e7d38e2fa78777e1411a6d38f1bc21
Keren Park <parkhk10@gmail.com>
2019-11-16 14:42:04 -0600

    update yml

--- src/components/Restaurant.test.js
+++ src/components/Restaurant.test.js
@@ -5,8 +5,8 @@ import Restaurant from './Restaurant';
 
 test('Restaurant should render passed states to display restaurant cards', () => {
     // render set-up
-    let key = "1"
-    let restaurant = {id: "1", name: "Koco Table", type: "Korean", price: 14, start: "1100", end: "1350",
+    const key = "1"
+    const restaurant = {id: "1", name: "Koco Table", type: "Korean", price: 14, start: "1100", end: "1350",
                       tables: [2, 4, 6, 10], party_size: ["small", "medium", "large"], vibes: ["happy_hour", "good_for_clients", "family_friendly", "team_bonding"]}
     let selectedRestaurants = [];
     let setSelectedRestaurants = jest.fn();
