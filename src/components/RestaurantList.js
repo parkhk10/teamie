ef83e754bee03de1e6921f0b165fc4ba782cb684
Keren Park <parkhk10@gmail.com>
2019-12-08 21:35:10 -0600

    change the title of app

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -95,7 +95,7 @@ const AppBar_header =  ({numPeople, setNumPeople, setBudget, setVibe}) => {
       <Container>
         <Toolbar>
           <Typography variant="h6" className={classes.title}>
-            Teamie - TravisCI
+            Teamie - CI/CD/Testing Activity Version
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
